b545d1fc3593b8ffabc03c526c994be368b46e68
team-cake <eka.cheuk@gmail.com>
2020-10-02 14:39:04 +0200

    Practicing objects types

--- 1/app.ts
+++ 1/app.ts
@@ -1,19 +1,10 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-	// console.log(typeof number1)
-	// if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-	// 	throw new Error('incorrect input!')
-	// }
-	const result = n1 + n2
-	if (showResult) {
-		console.log(phrase + result)
-	} else {
-		return result
-	}
+const person: {
+	name: string
+	number: number
+} = {
+	// const person = {
+	name: 'Alex',
+	number: 1,
 }
 
-const number1 = 5 // 5.0 doesn't matter
-const number2 = 2.8
-const printResult = true
-const resultPhrase = 'Result is: '
-
-add(number1, number2, printResult, resultPhrase)
+console.log(person.name)
