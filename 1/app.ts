f803d8d0beda072909dcabafad174f0685973e43
team-cake <eka.cheuk@gmail.com>
2020-10-02 15:09:42 +0200

    Practicing tuples

--- 1/app.ts
+++ 1/app.ts
@@ -2,11 +2,21 @@
 // 	name: string
 // 	number: number
 // } = {
-const person = {
+const person: {
+	name: string
+	number: number
+	hobbies: string[]
+	role: [number, string] // tuple type
+} = {
 	name: 'Alex',
 	number: 1,
 	hobbies: ['Skateboarding', 'Cooking'],
+	role: [2, 'developer'], // tuple type
 }
+
+// person.role.push('team lead')
+// person.role[2] = 10
+// person.role = [0, 'team lead', 'senior developer']
 
 let favoriteActivities: string[]
 favoriteActivities = ['Skateboarding']
@@ -15,4 +25,5 @@ console.log(person.name)
 
 for (const hobby of person.hobbies) {
 	console.log(hobby.toUpperCase())
+	// console.log(hobby.map()) // !!! ERROR !! As we know it's not possible to map over strings
 }
