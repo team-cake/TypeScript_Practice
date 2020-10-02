c0d2800363708e9b2f09542dee17fd6660edba76
team-cake <eka.cheuk@gmail.com>
2020-10-02 14:45:54 +0200

    Practicing array of strings

--- 1/app.ts
+++ 1/app.ts
@@ -1,10 +1,18 @@
-const person: {
-	name: string
-	number: number
-} = {
-	// const person = {
+// const person: {
+// 	name: string
+// 	number: number
+// } = {
+const person = {
 	name: 'Alex',
 	number: 1,
+	hobbies: ['Skateboarding', 'Cooking'],
 }
 
+let favoriteActivities: string[]
+favoriteActivities = ['Skateboarding']
+
 console.log(person.name)
+
+for (const hobby of person.hobbies) {
+	console.log(hobby.toUpperCase())
+}
