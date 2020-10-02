039331ed794d2e9d0f1ef9ea154fa55bc27ce8c1
team-cake <eka.cheuk@gmail.com>
2020-10-02 13:53:10 +0200

    throw an error to see what happens

--- 1/app.ts
+++ 1/app.ts
@@ -1,8 +1,12 @@
 function add(n1: number, n2: number) {
+	// console.log(typeof number1)
+	if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+		throw new Error('incorrect input!')
+	}
 	return n1 + n2
 }
 
-const number1 = 5
+const number1 = '5'
 const number2 = 2.8
 
 const result = add(number1, number2)
