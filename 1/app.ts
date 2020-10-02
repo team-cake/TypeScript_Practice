702db470de3870a05ba5b21602da9ba4e6544822
team-cake <eka.cheuk@gmail.com>
2020-10-02 14:04:41 +0200

    Adjusted the console log on line 8 so it's less prone to error

--- 1/app.ts
+++ 1/app.ts
@@ -3,16 +3,17 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
 	// if (typeof n1 !== 'number' || typeof n2 !== 'number') {
 	// 	throw new Error('incorrect input!')
 	// }
+	const result = n1 + n2
 	if (showResult) {
-		console.log(phrase, n1 + n2)
+		console.log(phrase + result)
 	} else {
-		return n1 + n2
+		return result
 	}
 }
 
 const number1 = 5 // 5.0 doesn't matter
 const number2 = 2.8
 const printResult = true
-const resultPhrase = 'Result is:'
+const resultPhrase = 'Result is: '
 
 add(number1, number2, printResult, resultPhrase)
