070aeba21450bc70a3d03a457180009d8f90bfd9
team-cake <eka.cheuk@gmail.com>
2020-10-02 14:02:41 +0200

    Add different types

--- 1/app.ts
+++ 1/app.ts
@@ -1,13 +1,18 @@
-function add(n1: number, n2: number) {
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
 	// console.log(typeof number1)
-	if (typeof n1 !== 'number' || typeof n2 !== 'number') {
-		throw new Error('incorrect input!')
+	// if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+	// 	throw new Error('incorrect input!')
+	// }
+	if (showResult) {
+		console.log(phrase, n1 + n2)
+	} else {
+		return n1 + n2
 	}
-	return n1 + n2
 }
 
-const number1 = '5'
+const number1 = 5 // 5.0 doesn't matter
 const number2 = 2.8
+const printResult = true
+const resultPhrase = 'Result is:'
 
-const result = add(number1, number2)
-console.log(result)
+add(number1, number2, printResult, resultPhrase)
