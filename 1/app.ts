d210de7c4c3fd5a7511ea7242bbfdedb30c87207
team-cake <eka.cheuk@gmail.com>
2020-10-03 14:50:44 +0200

    Function types and callbacks

--- 1/app.ts
+++ 1/app.ts
@@ -6,6 +6,11 @@ function printResult(num: number): void {
 	console.log('Result: ' + num)
 }
 
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+	const result = n1 + n2
+	cb(result)
+}
+
 printResult(add(5, 12))
 
 // let combineValues: Function
@@ -18,3 +23,7 @@ combineValues = add
 console.log(combineValues(8, 8))
 
 // let someValue: undefined
+
+addAndHandle(10, 20, (result) => {
+	console.log(result)
+})
