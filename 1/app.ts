ad3ef7a0b6e05306ebfcf648335cc8a6e7f8622d
team-cake <eka.cheuk@gmail.com>
2020-10-03 14:45:35 +0200

    Functions as types

--- 1/app.ts
+++ 1/app.ts
@@ -8,4 +8,13 @@ function printResult(num: number): void {
 
 printResult(add(5, 12))
 
+// let combineValues: Function
+let combineValues: (a: number, b: number) => number
+
+combineValues = add
+// combineValues = printResult
+// combineValues = 5
+
+console.log(combineValues(8, 8))
+
 // let someValue: undefined
