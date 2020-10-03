b49c849e34863f6c0364d9bfd956045c067fb9ca
team-cake <eka.cheuk@gmail.com>
2020-10-03 18:18:05 +0200

    Creating a class

--- 4/src/app.ts
+++ 4/src/app.ts
@@ -1,1 +1,13 @@
-// Code goes here!
+class Department {
+	name: string
+
+	constructor(n: string) {
+		this.name = n
+	}
+}
+
+const development = new Department('Development')
+
+console.log(development)
+
+// console.log('hello')
