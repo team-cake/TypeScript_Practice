5566bcccde2baf33b67842c7662a6be0ae08fed6
team-cake <eka.cheuk@gmail.com>
2020-10-03 15:05:50 +0200

    The 'never' type

--- 1/app.ts
+++ 1/app.ts
@@ -6,3 +6,10 @@ userInput = 'Alex'
 if (typeof userInput === 'string') {
 	userName = userInput
 }
+
+function generateError(message: string, code: number): never {
+	throw { message: message, errorCode: code }
+	// while (true) {}
+}
+
+generateError('An error occured', 500)
