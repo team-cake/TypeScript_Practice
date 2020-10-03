6860f48b895858d948284536eeed56248e44ea41
team-cake <eka.cheuk@gmail.com>
2020-10-03 17:28:36 +0200

    double checking on arrow functions

--- 3/src/app.ts
+++ 3/src/app.ts
@@ -4,11 +4,11 @@ let age = 20
 
 age = 29
 
-function add(a: number, b: number) {
-	let result
-	result = a + b
-	return result
-}
+// function add(a: number, b: number) {
+// 	let result
+// 	result = a + b
+// 	return result
+// }
 
 // if (age > 20) {
 // 	let isOld = true
@@ -17,3 +17,20 @@ function add(a: number, b: number) {
 // console.log(isOld)
 
 // console.log(result)
+
+const add = (a: number, b: number) => {
+	return a + b
+}
+
+console.log(add(2, 5))
+
+const printOutput: (a: number | string) => void = (output) =>
+	console.log(output)
+
+const button = document.querySelector('button')
+
+if (button) {
+	button.addEventListener('click', (event) => console.log(event))
+}
+
+printOutput(add(5, 2))
