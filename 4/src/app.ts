f49797f52c5b27996ccda0670a826452ae457a2f
team-cake <eka.cheuk@gmail.com>
2020-10-03 18:42:58 +0200

    Practicing shorthand initialization

--- 4/src/app.ts
+++ 4/src/app.ts
@@ -1,13 +1,15 @@
 class Department {
-	public name: string
+	// private id: string
+	// private name: string
 	private employees: string[] = []
 
-	constructor(n: string) {
-		this.name = n
+	constructor(private id: string, public name: string) {
+		// this.id = id
+		// this.name = n
 	}
 
 	describe(this: Department) {
-		console.log('Department: ' + this.name)
+		console.log(`Department (${this.id}: ${this.name})`)
 	}
 
 	addEmployee(employee: string) {
@@ -21,7 +23,7 @@ class Department {
 	}
 }
 
-const development = new Department('Development')
+const development = new Department('A1', 'Development')
 
 development.addEmployee('Alex')
 development.addEmployee('Alexa')
