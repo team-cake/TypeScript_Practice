a49d48e0b4a825a6452731167005aa39387d8a98
team-cake <eka.cheuk@gmail.com>
2020-10-03 18:30:37 +0200

    Constructor functions and the 'this' keyword

--- 4/src/app.ts
+++ 4/src/app.ts
@@ -4,10 +4,17 @@ class Department {
 	constructor(n: string) {
 		this.name = n
 	}
+
+	describe(this: Department) {
+		console.log('Department: ' + this.name)
+	}
 }
 
 const development = new Department('Development')
 
-console.log(development)
+// console.log(development)
+development.describe()
 
-// console.log('hello')
+const developmentCopy = { name: 's', describe: development.describe }
+
+developmentCopy.describe()
