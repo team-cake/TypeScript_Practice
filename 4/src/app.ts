4f3824b9d98b2de09e7c81c4e0a7aaa1d02c2212
team-cake <eka.cheuk@gmail.com>
2020-10-03 18:38:06 +0200

    Private and public access modifiers

--- 4/src/app.ts
+++ 4/src/app.ts
@@ -1,5 +1,6 @@
 class Department {
-	name: string
+	public name: string
+	private employees: string[] = []
 
 	constructor(n: string) {
 		this.name = n
@@ -8,13 +9,31 @@ class Department {
 	describe(this: Department) {
 		console.log('Department: ' + this.name)
 	}
+
+	addEmployee(employee: string) {
+		// validation?
+		this.employees.push(employee)
+	}
+
+	printEmployeeInformation() {
+		console.log(this.employees.length)
+		console.log(this.employees)
+	}
 }
 
 const development = new Department('Development')
 
-// console.log(development)
+development.addEmployee('Alex')
+development.addEmployee('Alexa')
+
+// development.employees[2] = 'Angela'
+
 development.describe()
+development.name = 'NEW NAME'
+development.printEmployeeInformation()
 
-const developmentCopy = { name: 's', describe: development.describe }
+// console.log(development)
 
-developmentCopy.describe()
+// const developmentCopy = { name: 's', describe: development.describe }
+
+// developmentCopy.describe()
