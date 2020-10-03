45b806d39ca9490b4afe84e3cfa09de993cc4ba0
team-cake <eka.cheuk@gmail.com>
2020-10-03 17:21:54 +0200

    let & const practicing

--- 3/src/app.ts
+++ 3/src/app.ts
@@ -1,1 +1,19 @@
-// Code goes here!
+const userName = 'Alex'
+// userName = 'Alex C'
+let age = 20
+
+age = 29
+
+function add(a: number, b: number) {
+	let result
+	result = a + b
+	return result
+}
+
+// if (age > 20) {
+// 	let isOld = true
+// }
+
+// console.log(isOld)
+
+// console.log(result)
