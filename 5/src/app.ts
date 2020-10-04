6f9c8b3b5c7ab93ccb8af3ae4fff857f366ccb5d
team-cake <eka.cheuk@gmail.com>
2020-10-04 12:55:03 +0200

    Practicing intersection types

--- 5/src/app.ts
+++ 5/src/app.ts
@@ -1,1 +1,25 @@
-// Code goes here!
+type Admin = {
+	name: string
+	privileges: string[]
+}
+
+type Employee = {
+	name: string
+	startDate: Date
+}
+// interface ElevatedEmployee extends Employee, Admin {}
+
+type ElevatedEmployee = Admin & Employee
+
+const e1: ElevatedEmployee = {
+	name: 'Alex',
+	privileges: ['create-software'],
+	startDate: new Date(),
+}
+
+type Combinable = string | number
+type Numeric = number | boolean
+
+type Universal = Combinable & Numeric
+
+console.log(e1)
