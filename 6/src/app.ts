851b03fdb88233ad073e505d6c6bc5ffa76300a8
team-cake <eka.cheuk@gmail.com>
2020-10-04 15:17:42 +0200

    Practicing built-in generics

--- 6/src/app.ts
+++ 6/src/app.ts
@@ -1,1 +1,12 @@
-// Code goes here!
+const names: Array<string> = ['Alex', 'Micah']; // string[], this is a generic type
+// names[0].split(' ');
+
+const promise: Promise<string> = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		resolve('This is done');
+	}, 2000);
+});
+
+promise.then((data) => {
+	data.split(' ');
+});
