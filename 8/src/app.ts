73e60b7f2251d9712cb0cdf3fabb23b1ff3db1f0
team-cake <eka.cheuk@gmail.com>
2020-10-06 16:52:36 +0200

    Trying to set up a project state constant

--- 8/src/app.ts
+++ 8/src/app.ts
@@ -1,3 +1,32 @@
+// Project State Management Class
+
+class ProjectState {
+	private projects: any[] = [];
+	private static instance: ProjectState;
+
+	private constructor() {}
+
+	static getInstance() {
+		if (this.instance) {
+			return this.instance;
+		}
+		this.instance = new ProjectState();
+		return this.instance;
+	}
+
+	addProject(title: string, description: string, numOfPeople: number) {
+		const newProject = {
+			id: Math.random().toString(),
+			title: title,
+			description: description,
+			people: numOfPeople,
+		};
+		this.projects.push(newProject);
+	}
+}
+
+const projectState = ProjectState.getInstance();
+
 // Validate
 interface Validatable {
 	value: string | number;
@@ -171,7 +200,6 @@ class ProjectInput {
 		const userInput = this.gatherUserInput();
 		if (Array.isArray(userInput)) {
 			const [title, desc, people] = userInput;
-			console.log(title, desc, people);
 			this.clearInputs();
 		}
 	}
