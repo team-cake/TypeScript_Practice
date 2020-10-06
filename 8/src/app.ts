34a2b485919c78bd8883e822d65cc41df5e70387
team-cake <eka.cheuk@gmail.com>
2020-10-06 18:04:13 +0200

    Setting some generics to have the component class reusable

--- 8/src/app.ts
+++ 8/src/app.ts
@@ -110,6 +110,31 @@ function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
 	return adjDescriptor;
 }
 
+// Component Base Class
+class Component<T extends HTMLElement, U extends HTMLElement> {
+	templateElement: HTMLTemplateElement;
+	hostElement: T;
+	element: U;
+
+	constructor(
+		templateId: string,
+		hostElementId: string,
+		newElementId?: string
+	) {
+		this.templateElement = document.getElementById(
+			templateId
+		)! as HTMLTemplateElement;
+		this.hostElement = document.getElementById(hostElementId)! as T;
+
+		const importedNode = document.importNode(
+			this.templateElement.content,
+			true
+		);
+		this.element = importedNode.firstElementChild as U;
+		this.element.id = `${this.type}-projects`;
+	}
+}
+
 // ProjectList Class
 class ProjectList {
 	templateElement: HTMLTemplateElement;
