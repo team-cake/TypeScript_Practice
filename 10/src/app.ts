9c31d72b994b2ee5b4a82875dd5fa1877d8fb701
team-cake <eka.cheuk@gmail.com>
2020-10-14 15:00:20 +0200

    Set up so the input address is being searched on google api

--- 10/src/app.ts
+++ 10/src/app.ts
@@ -1,12 +1,27 @@
-const form = document.querySelector('form')!;
+import axios from 'axios';
 
+const form = document.querySelector('form')!;
 const addressInput = document.getElementById('address')! as HTMLInputElement;
+
+const GOOGLE_API_KEY = 'AIzaSyCfahO-LwrU4a8_48BHiE6kJkp6xVlMS64';
 
 function searchAddressHandler(event: Event) {
 	event.preventDefault();
 	const enteredAddress = addressInput.value;
 
 	// send this to Google's API!
+	axios
+		.get(
+			`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+				enteredAddress
+			)}&key=${GOOGLE_API_KEY}`
+		)
+		.then((response) => {
+			console.log(response);
+		})
+		.catch((err) => {
+			console.log(err);
+		});
 }
 
 form.addEventListener('submit', searchAddressHandler);
