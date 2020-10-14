effe95f019f3849066247307c264742acd6935a1
team-cake <eka.cheuk@gmail.com>
2020-10-14 13:09:21 +0200

    Exploring class-transformer

--- 9/src/app.ts
+++ 9/src/app.ts
@@ -1,3 +1,21 @@
-import _ from 'lodash';
+import 'reflect-metadata';
+import { plainToClass } from 'class-transformer';
 
-console.log(_.shuffle([1, 2, 3]));
+import { Product } from './product.model';
+
+const products = [
+	{ title: 'A carpet', price: 29.99 },
+	{ title: 'A Book', price: 10.99 },
+];
+
+// const p1 = new Product('A book', 12.99);
+
+// const loadedProducts = products.map((prod) => {
+// 	return new Product(prod.title, prod.price);
+// });
+
+const loadedProducts = plainToClass(Product, products);
+
+for (const prod of loadedProducts) {
+	console.log(prod.getInformation());
+}
