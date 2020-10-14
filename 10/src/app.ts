0cfe53a11466fcf50fb97f97f203c76b893df494
team-cake <eka.cheuk@gmail.com>
2020-10-14 15:06:59 +0200

    Added response type so TS knows how to complain, also an error message.

--- 10/src/app.ts
+++ 10/src/app.ts
@@ -5,21 +5,30 @@ const addressInput = document.getElementById('address')! as HTMLInputElement;
 
 const GOOGLE_API_KEY = 'AIzaSyCfahO-LwrU4a8_48BHiE6kJkp6xVlMS64';
 
+type GoogleGeocodingResponse = {
+	results: { geomatry: { location: { lat: number; lng: number } } }[];
+	status: 'OK' | 'ZERO_RESULTS';
+};
+
 function searchAddressHandler(event: Event) {
 	event.preventDefault();
 	const enteredAddress = addressInput.value;
 
 	// send this to Google's API!
 	axios
-		.get(
+		.get<GoogleGeocodingResponse>(
 			`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
 				enteredAddress
 			)}&key=${GOOGLE_API_KEY}`
 		)
 		.then((response) => {
+			if (response.data.status !== 'OK') {
+				throw new Error('Could not fetch location!');
+			}
 			console.log(response);
 		})
 		.catch((err) => {
+			alert(err.message);
 			console.log(err);
 		});
 }
