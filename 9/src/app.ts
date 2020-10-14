4c5bf6ccab40cb51c536b113dce52ad587c305c1
team-cake <eka.cheuk@gmail.com>
2020-10-14 13:22:37 +0200

    Exploring class-validator

--- 9/src/app.ts
+++ 9/src/app.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata';
 import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
 
 import { Product } from './product.model';
 
@@ -7,6 +8,16 @@ const products = [
 	{ title: 'A carpet', price: 29.99 },
 	{ title: 'A Book', price: 10.99 },
 ];
+
+const newProd = new Product('', -5.99);
+validate(newProd).then((errors) => {
+	if (errors.length > 0) {
+		console.log('VALIDATION ERRORS!');
+		console.log(errors);
+	} else {
+		console.log(newProd.getInformation());
+	}
+});
 
 // const p1 = new Product('A book', 12.99);
 
