ec7a71dde4c3584491f7a2174bd46537e74f2fce
team-cake <eka.cheuk@gmail.com>
2020-10-15 12:41:04 +0200

    Simple todo add

--- 11/src/components/NewTodo.tsx
+++ 11/src/components/NewTodo.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const NewTodo: React.FC = () => {
-	return <form></form>;
+	return (
+		<form>
+			<div>
+				<label htmlFor='todo-text'>Todo Text</label>
+				<input type='text' id='todo-text' />
+			</div>
+			<button type='submit'>ADD TODO</button>
+		</form>
+	);
 };
 
 export default NewTodo;
