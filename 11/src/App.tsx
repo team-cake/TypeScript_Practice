40c70066276dc077f9a103adcfda732c352f4f5a
team-cake <eka.cheuk@gmail.com>
2020-10-15 12:36:52 +0200

    Added NewTodo

--- 11/src/App.tsx
+++ 11/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Todolist from './components/Todolist';
+import NewTodo from './components/NewTodo';
 
 const App: React.FC = () => {
 	const todos = [{ id: 's1', text: 'Land ollie' }];
 
 	return (
 		<div className='App'>
+			<NewTodo />
 			<Todolist items={todos} />
 		</div>
 	);
