0f2d5b2b6db41fb7a7e69b68bf804ec5b31a0c04
team-cake <eka.cheuk@gmail.com>
2020-10-15 11:08:48 +0200

    Imported Todolist.tsx in App.tsx

--- 11/src/App.tsx
+++ 11/src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import Todolist from './components/Todolist';
 
 const App: React.FC = () => {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<Todolist />
+		</div>
+	);
 };
 
 export default App;
